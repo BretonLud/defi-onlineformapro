251b54a9a3e4f284970c39439cfd5f5e41812ef4
root <root@PORT202105-018.localdomain>
2021-09-17 14:21:07 +0200

    [update]portfolio

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="defi Online Forma Pro" content="Voici le réslutat de mon défi">
+    <meta name="description" content="Voici le réslutat de mon défi pour OnlineFormaPro">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;900&display=swap" rel="stylesheet"> 
